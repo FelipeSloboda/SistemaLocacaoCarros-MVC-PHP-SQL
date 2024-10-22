477e231feeab3aa58703123bff2c6c893d7c037a
FelipeSloboda <felipe.ti.sloboda@gmail.com>
2024-10-22 11:03:57 -0300

    Ajuste string connection DB
    Ajuste string connection DB

--- lib/database/connection.php
+++ lib/database/connection.php
@@ -1,7 +1,7 @@
 <?php 
     abstract class Connection{
         private static $conn;
-        public static function getConn(){
+        public static function getStringConn(){
             if(self::$conn == null){
                 self::$conn = new PDO("mysql:host=localhost; dbname=locacao","root","");
             }
