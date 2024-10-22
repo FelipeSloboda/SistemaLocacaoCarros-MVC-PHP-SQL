6d7d63d24b4e5831c9f8eb116a943e9f41af940d
FelipeSloboda <felipe.ti.sloboda@gmail.com>
2024-10-22 11:02:40 -0300

    Ajustes no Core, para reconhecimento de ID
    Ajustes no Core, para reconhecimento de ID

--- app/core/Core.php
+++ app/core/Core.php
@@ -2,9 +2,7 @@
 
 class Core{
     public function start($getUrl){
-        $id = null;
-        $acao = null;
-        $controller = null;
+        $id = $acao = $controller = null;
         
         //PAGINA
         if(isset($getUrl['pagina'])){$controller = ucfirst($getUrl['pagina'].'Controller');} 
@@ -17,11 +15,8 @@ class Core{
         if(isset($getUrl['metodo'])){$acao = $getUrl['metodo'];} 
         else{$acao = "index";}
 
-        //METODO (TRATAMENTO DE METODO INEXISTENTE) 
-        if(!method_exists($acao,'')) {$acao = "index";} 
-
         //ID
-        if(isset($urlGet['id'])){$id = $getUrl['id'];}
+        if((isset($getUrl['id'])) && ($getUrl['id'] != null)) {$id = $getUrl['id'];}
 
         //DIRECIONAMENTO PARA A CONTROLLER
         call_user_func_array(array(new $controller, $acao), array($id));
