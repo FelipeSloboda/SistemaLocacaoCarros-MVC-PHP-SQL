53f2a88aefac77e41ecc4b32a6b7ace936c85159
FelipeSloboda <felipe.ti.sloboda@gmail.com>
2024-10-22 11:03:33 -0300

    Adicionado dependencias na index
    Adicionado dependencias na index

--- index.php
+++ index.php
@@ -2,12 +2,21 @@
 
 // REQUISICAO DAS CLASSES UTILIZADAS (MVC)
 require_once "app/core/Core.php";
+
 require_once "app/controller/ErroController.php";
 require_once "app/controller/HomeController.php";
+require_once "app/controller/ClienteController.php";
+require_once "app/controller/VeiculoController.php";
+require_once "app/controller/LocacaoController.php";
 
 require_once "app/model/Home.php";
+require_once "app/model/Cliente.php";
+require_once "app/model/Veiculo.php";
+require_once "app/model/Locacao.php";
 
 require_once "lib/database/connection.php";
+
+require_once "vendor/autoload.php"; // TWIG
 
 //CARREGA A PAGINA ESTRUTURA.HTML
 $estrutura = file_get_contents('app/template/estrutura.html'); 
